7ef9b790449f232b4ec81ac6c844c5240c188263
pettsan <pettsan@ntnu.no>
2024-10-09 00:56:55 +0200

    #1, #5. Added weapon assets, added flipbooks for fire and idle pistol, added pistol to hud, added weaon bobbing when walking, missing shooting functionality

---
+++ Source/BoomerShooter/FPSAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "FPSAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOOMERSHOOTER_API UFPSAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
