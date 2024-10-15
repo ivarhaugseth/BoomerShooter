648fc851abd92da6287f5dd2f16d6ceffe5b0dbc
pettsan <pettsan@ntnu.no>
2024-10-16 00:21:18 +0200

    Finished adding health and armor pickups. Also temporary health and armor values have been added to the HUD before the finished HUD is implemented.

--- Source/BoomerShooter/FPSAttributeSet.cpp
+++ Source/BoomerShooter/FPSAttributeSet.cpp
@@ -13,12 +13,28 @@ void UFPSAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, f
 
 bool UFPSAttributeSet::PreGameplayEffectExecute(FGameplayEffectModCallbackData& Data)
 {
-	float ArmorDamage = FMath::Abs(Data.EvaluatedData.Magnitude);
+	float AbsoluteMagnitude = FMath::Abs(Data.EvaluatedData.Magnitude);
 
-	// Case where the damage is greater than the armor value and so the armor is destroyed and the remaining damage is applied to the health
-	if (Data.EvaluatedData.Attribute == GetArmorAttribute() && ArmorDamage > GetArmor() && Data.EvaluatedData.Magnitude < 0)
+	if (Data.EvaluatedData.Magnitude > 0)
 	{
-		Health.SetCurrentValue(Health.GetCurrentValue() - (ArmorDamage - GetArmor()));
+		if (Data.EvaluatedData.Attribute == GetArmorAttribute() && GetArmor() + AbsoluteMagnitude >= 100)
+		{
+			SetArmor(100);
+			return false;
+		}
+		if (Data.EvaluatedData.Attribute == GetHealthAttribute() && GetHealth() + AbsoluteMagnitude >= 100)
+		{
+			SetHealth(100);
+			return false;
+		}
+	}
+	else
+	{
+		// Case where the damage is greater than the armor value and so the armor is destroyed and the remaining damage is applied to the health
+		if (Data.EvaluatedData.Attribute == GetArmorAttribute() && AbsoluteMagnitude > GetArmor())
+		{
+			Health.SetCurrentValue(Health.GetCurrentValue() - (AbsoluteMagnitude - GetArmor()));
+		}
 	}
 
 	return true;
